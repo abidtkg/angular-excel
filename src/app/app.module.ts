a134baa91e573a9c69a2057313a4950a7803dd39
Abid Hasan <abidtkg@gmail.com>
2024-10-21 03:39:04 +0600

    update angular core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,28 +7,22 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import {MatButtonModule} from '@angular/material/button';
 import { UploadExcelComponent } from './upload-excel/upload-excel.component';
 import { ApiService } from './api.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ProductUploadComponent } from './product-upload/product-upload.component';
 import { ExcelExportComponent } from './excel-export/excel-export.component';
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    UploadExcelComponent,
-    ProductUploadComponent,
-    ExcelExportComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatButtonModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideAnimationsAsync(),
-    ApiService
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        UploadExcelComponent,
+        ProductUploadComponent,
+        ExcelExportComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        MatButtonModule], providers: [
+        provideAnimationsAsync(),
+        ApiService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
